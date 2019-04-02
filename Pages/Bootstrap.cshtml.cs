ae8ec51b598790883999dd0a1e80d7ff45973dad
Angelicaalonzo <31522129+Angelicaalonzo@users.noreply.github.com>
2019-04-01 19:10:07 -0500

    finished

--- Pages/Bootstrap.cshtml.cs
+++ Pages/Bootstrap.cshtml.cs
@@ -5,22 +5,46 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Lab10.Pages
 {
     public class BootstrapModel : PageModel
     {
+        [BindProperty]
+        [Display(Name = "First Name")]
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
+        public string FirstName {get; set;}
+        [BindProperty]
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
+        [Display(Name = "Last Name ")]
+        public string LastName{get; set;}
+
+        [BindProperty]
+        [Display(Name = "Credit Card")]
+        [Required]
+        [CreditCard]
+        public string CardNumber{get; set;}
+
+
         private readonly ILogger _log;
         public BootstrapModel(ILogger<IndexModel> log)
             {
                 _log = log;
             }
  
-        public void OnPost()
-            {
-                _log.LogInformation("HTTP Post Request");
+        public void OnGet()
+        {
+            
+        }
 
-            }
+        public void OnPost()
+        {
+            _log.LogWarning($"{FirstName}{LastName}");
+        }
 
     }
 }
